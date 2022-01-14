53a4801d662e687ffaa35b8e5e1c32a955e57450
jia <fire_mars_j@163.com>
2022-01-14 16:12:47 +0800

    change input-number-undefined-in-form-5049.vue

--- src/pages/202112/input-number-undefined-in-form-5049.vue
+++ src/pages/202112/input-number-undefined-in-form-5049.vue
@@ -1,51 +1,47 @@
 <template>
-  <el-form ref="Form" label-suffix=":" :model="ruleForm">
-    <el-form-item label="数字" prop="num" :rules="ruleForm.rules">
+  <el-form ref="ruleForm" label-suffix=":" :model="form">
+    <el-form-item
+      label="数字"
+      prop="num"
+      :rules="[{ required: true, message: '请输入数字' }]"
+    >
       <el-input-number
-        v-model="ruleForm.num"
-        :controls="false"
+        v-model="form.num"
+        placeholder="please input number"
       ></el-input-number>
     </el-form-item>
     <el-form-item label="当前输入的数字" prop="num">
-      {{ ruleForm.num }}
+      {{ form.num }}
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="submitForm()"> 提交 </el-button>
     </el-form-item>
   </el-form>
-  <el-button @click="reset()">reset</el-button>
+  <el-button @click="setNull"> setNull </el-button>
+  <el-button @click="setUndefined"> setUndefined </el-button>
 </template>
 
-<script lang="ts">
-import { ref, defineComponent } from "vue";
-export default defineComponent({
-  setup() {
-    const ruleForm = ref({
-      num: undefined,
-      rules: [{ required: true, message: "请输入数字" }],
-    });
-    const Form = ref();
-    const submitForm = () => {
-      Form.value.validate((valid: any) => {
-        if (valid) {
-          alert(`submit success with ${ruleForm.value.num}`);
-        } else {
-          alert(`submit error`);
-        }
-        return false;
-      });
-    };
-    const reset = () => {
-      ruleForm.value.num = undefined;
-    };
-    return {
-      ruleForm,
-      Form,
-      submitForm,
-      reset,
-    };
-  },
+<script setup lang="ts">
+import { ref } from "vue";
+const ruleForm = ref();
+const form = ref({
+  num: undefined,
 });
+function submitForm() {
+  ruleForm.value.validate((valid: any) => {
+    if (valid) {
+      alert(`submit success with: ${form.value.num}`);
+    } else {
+      alert(`submit failed`);
+    }
+  });
+}
+function setNull() {
+  form.value.num = null;
+}
+function setUndefined() {
+  form.value.num = undefined;
+}
 </script>
 
 <style lang="scss">
