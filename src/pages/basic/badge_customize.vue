8401332c706cc88a0238bc3b10a72d49a0e8f17e
jia <fire_mars_j@163.com>
2022-02-16 10:32:34 +0800

    add badge_customize.vue

---
+++ src/pages/basic/badge_customize.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-badge value="new" class="item">
+    <el-button size="small">comments</el-button>
+  </el-badge>
+  <el-badge value="hot" class="item">
+    <el-button size="small">replies</el-button>
+  </el-badge>
+</template>
+
+<script>
+export default {
+  name: "badge_customize",
+};
+</script>
+
+<style scoped></style>
